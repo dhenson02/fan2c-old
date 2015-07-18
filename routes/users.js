e734fcc95e61e1a81d35085e518343e403caa205
Deryck Henson <deryck.henson@gmail.com>
2015-07-18 16:33:49 -0500

    remoteapi added to ignore, tested URL

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var app = require('../remote-api'); // <-- keeps the URL and league ID handy
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send("yeah" + app.remoteURL);
 });
 
 module.exports = router;

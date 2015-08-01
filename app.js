b3129678ae295ccfb08aaf4ecb05fd3fa31446e6
Deryck Henson <deryck.henson@gmail.com>
2015-08-01 00:56:39 -0500

    api key is single string for now

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var api = require('./remote-api');
+//var api = require('./remote-api');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/sense', api);
+//app.use('/q', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var api = require('../remote-api'); // <-- keeps the URL and league ID handy
 
 /* GET users listing. */
 router.get('/users', function(req, res, next) {
-  res.send("yeah" + api.remoteURL);
+  res.send("yeah" + api);
 });
 
 module.exports = router;

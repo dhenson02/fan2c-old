3bd41f6d5ae55cc9d7c8722461fac3f2840904ad
Deryck Henson <deryck.henson@gmail.com>
2015-08-01 11:59:58 -0500

    api.players will hold the players collection for now.

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var db = require("../bin/db");
+var api = require("../remote-api");
 
 router.get('/', function ( req, res ) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/:q', function ( req, res ) {
+router.get('/nbw/:q', function ( req, res ) {
   var typeStr = req.params.q;
   console.log(typeStr);
-  db.pullAll(typeStr, function ( err, docs ) {
-    res.send(err);
-  });
+  res.send(api.players);
 });
 
 module.exports = router;

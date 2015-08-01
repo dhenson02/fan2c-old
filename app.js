2a40eb4fab1c9c936efc4f3f83b58a27346ec2d1
Deryck Henson <deryck.henson@gmail.com>
2015-08-01 00:40:00 -0500

    adjusted requires and corrected main app.use

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./remote-api');
 
 var app = express();
 
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/sense', users);
+app.use('/sense', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var app = require('../remote-api'); // <-- keeps the URL and league ID handy
+var api = require('../remote-api'); // <-- keeps the URL and league ID handy
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send("yeah" + app.remoteURL);
+router.get('/users', function(req, res, next) {
+  res.send("yeah" + api.remoteURL);
 });
 
 module.exports = router;
